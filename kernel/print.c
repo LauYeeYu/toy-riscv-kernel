55a92721493b26fa6e6b93a1c19dba969558a4bc
Lau Yee-Yu <liuyiyu2003@126.com>
2023-04-12 00:07:28 +0800

    feat(print): print int

--- kernel/print.c
+++ kernel/print.c
@@ -16,3 +16,21 @@ void print_string(const char *str) {
     }
     release(&lock);
 }
+
+void print_int(uint64 x) {
+    acquire(&lock);
+    if (x == 0) {
+        uart_putc_sync('0');
+    } else {
+        char buf[16];
+        int i = 0;
+        while (x > 0) {
+            buf[i++] = '0' + x % 10;
+            x /= 10;
+        }
+        while (i > 0) {
+            uart_putc_sync(buf[--i]);
+        }
+    }
+    release(&lock);
+}
--- kernel/print.h
+++ kernel/print.h
@@ -1,6 +1,9 @@
 #ifndef TOY_RISCV_KERNEL_KERNEL_PRINT_H
 #define TOY_RISCV_KERNEL_KERNEL_PRINT_H
 
+#include "types.h"
+
 void print_string(const char *str);
+void print_int(uint64 x);
 
 #endif //TOY_RISCV_KERNEL_KERNEL_PRINT_H
