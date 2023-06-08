c2db5b0fb4cbb42caf8e06d0172bf6add3c4028a
Lau Yee-Yu <liuyiyu2003@126.com>
2023-06-08 21:36:50 +0800

    rerfactor(elf): add interface in header file

--- kernel/elf.h
+++ kernel/elf.h
@@ -3,9 +3,14 @@
 
 #include <elf.h>
 
-enum stack_state {
-    EXECUTABLE,
-    NON_EXECUTABLE,
-};
+#include "process.h"
+
+/**
+ * Load the ELF file into the memory.
+ * @param elf the ELF file
+ * @param task the task_struct of the process
+ * @return 0 if success, -1 if failed
+ */
+int load_elf(void *elf, struct task_struct *task);
 
 #endif //TOY_RISCV_KERNEL_KERNEL_ELF_H
