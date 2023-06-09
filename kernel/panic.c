ffb62f5c2935547a4af10b2bd8c53326dc077c7a
Lau Yee-Yu <liuyiyu2003@126.com>
2023-06-09 14:09:11 +0800

    fix(panic): panic should turn off interrupt

--- kernel/panic.c
+++ kernel/panic.c
@@ -1,8 +1,10 @@
 #include "print.h"
+#include "riscv.h"
 
 volatile int panicked = 0;
 
 __attribute__((noreturn)) void panic(const char *msg) {
+    interrupt_off();
     print_string("panic: ");
     print_string(msg);
     print_string("\n");
