cbd39504a1a3a2a8f3a7c0ada7bebd737bd12fa9
Lau Yee-Yu <liuyiyu2003@126.com>
2023-06-15 02:45:26 +0800

    feat(user): library function for string

--- user/ulib.h
+++ user/ulib.h
@@ -3,6 +3,18 @@
 
 #include "system.h"
 
+#define max(a, b) ({                             \
+    typeof(a) __max1__ = (a);                    \
+    typeof(b) __max2__ = (b);                    \
+    (void)(&__max1__ == &__max2__);              \
+    __max1__ > __max2__ ? __max1__ : __max2__;})
+
+#define min(a, b) ({                             \
+    typeof(a) __min1__ = (a);                    \
+    typeof(b) __min2__ = (b);                    \
+    (void)(&__min1__ == &__min2__);              \
+    __min1__ < __min2__ ? __min1__ : __min2__;})
+
 static inline void *memset(void *dst, int c, size_t n) {
     char *pos = (char *) dst;
     while (n--) {
@@ -34,6 +46,36 @@ while (*s1 != 0 && *s1 == *s2) {
         s2++;
     }
     return (int) ((unsigned char) *s1 - (unsigned char) *s2);
+}
+
+static inline void strcpy(char *dest, const char *src, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        dest[i] = src[i];
+    }
+    dest[size] = '\0';
+}
+
+static inline char *strtok(char *str, const char *delim) {
+    static char *last = NULL;
+    if (str != NULL) {
+        last = str;
+    }
+    if (last == NULL) {
+        return NULL;
+    }
+    char *ret = last;
+    while (*last != '\0') {
+        for (size_t i = 0; delim[i] != '\0'; i++) {
+            if (*last == delim[i]) {
+                *last = '\0';
+                last++;
+                return ret;
+            }
+        }
+        last++;
+    }
+    last = NULL;
+    return ret;
 }
 
 /**
